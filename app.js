342a849544d562744ca0d00fbcfdb5ec715f2713
RealNabeal <abdina@sheridancollege.ca>
2023-10-02 22:40:05 -0400

    Implement card shuffling, game board initialization, and card flip event; Add basic grid and card styles

--- app.js
+++ app.js
@@ -14,4 +14,77 @@ const cardArray = [
         name: 'jellyfish',
         img: 'images/jellyfish.jpeg',
     },
+    {
+        name: 'meerkat',
+        img: 'meerkat.jpeg',
+    },
+    {
+        name: 'puppy',
+        img: 'puppy.jpeg',
+    },
+    {
+        name: 'pony',
+        img: 'pony.jpeg',
+    },
+    //Duplicate since we need a set of cards
+    {
+        name: 'cat',
+        img: 'images/cat.jpeg',
+    },
+
+    {
+        name: 'goat',
+        img: 'images/goat.jpeg',
+    },
+    {
+        name: 'jellyfish',
+        img: 'images/jellyfish.jpeg',
+    },
+    {
+        name: 'meerkat',
+        img: 'meerkat.jpeg',
+    },
+    {
+        name: 'puppy',
+        img: 'puppy.jpeg',
+    },
+    {
+        name: 'pony',
+        img: 'pony.jpeg',
+    },
+    
 ]
+
+
+//randomly shuffle the card array
+cardArray.sort(() => 0.5 - Math.random())
+
+const grid = document.querySelector('#grid')
+
+const pickedCard = []
+
+/**
+ * Initializes and populates the game board with card images.
+ * Each card is an image element with a default pattern and is
+ * associated with an index in the cardArray through a data-id attribute.
+ */
+function createBoard(){
+    for(let i = 0; i < cardArray.length; i++){
+        const card = document.createElement('img')
+        card.setAttribute('src','images/pattern.jpeg')
+        card.setAttribute('data-id', i)
+        console.log(card, i)
+        card.addEventListener('click', flipCard)
+        grid.appendChild(card)
+    }
+}
+
+createBoard()
+
+function flipCard(){
+    console.log(cardArray)
+    const cardId = this.getAttribute('data-id')
+    console.log(cardArray[cardId].name)
+    pickedCard.push(cardArray.name)
+    console.log('clicked', cardId)
+}
